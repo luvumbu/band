513aeb951b8cfb6952dca0a5d7ec79cdd623a34c
Luvumbu <luvumbu.n@gmail.com>
2020-05-24 09:05:38 +0200

    changement total de larchitechture implementation de la methode bokonzi

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
-<!-- 𝕨𝕨𝕨.𝔹𝕠𝕜𝕠𝕟𝕫𝕚.𝕔𝕠𝕞  𝖞𝖉𝖊𝖓𝖌𝖆 𝕿𝖚𝖛𝖚𝖒𝖇𝖚   ★☆♨♫✈❤
-    //  ★   =   début de l'information
-    //  ☆   =   fin de l'information
-    //  ♨   =   Code de l'information premier letre du fichier et dernier lettre du fichier suivi du numéro du commentaire.
-    //  ♫   = connexion css all suivi du nom du fichier 
-    //  ✈   ♨=#ix   Je suis dans la racine du fichier   ♨=#ix  
-    //  
-    //
-𝕨𝕨𝕨.𝔹𝕠𝕜𝕠𝕟𝕫𝕚.𝕔𝕠𝕞  𝖞𝖉𝖊𝖓𝖌𝖆 𝕿𝖚𝖛𝖚𝖒𝖇𝖚   ★☆♨♫✈❤   -->
+<?php
+    session_start() ; 
+?>
 <!DOCTYPE html>
-    <html lang="fr">
-            <head>
-            <script src="src/app/all/bkz.js"></script><!--  ★♨=ls0001  ☆   -->
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        </head>
-          <title>Document</title>
-            <body id="body">
-                <script type="text/javascript">
-                    window.onload = document.getElementById('body').style.display='none';
-                //  ★   n'affiche pas la page pendant le chargement debut ♨=#ix0001
-                </script>
-            <?php 
-                require "app.php" ;
-                //    ★   Ajoute le fichier app.php ♨=#ix0002 ☆    
-            ?>
-        </body>
-        <script type="text/javascript">
-            window.onload = document.getElementById('body').style.display='block';
-            //  affiche la page apres le chargement ♨=#ix0001   ☆
-        </script>
-    </html>
-<style>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" type="image/png" href="Class/favicon/favicon.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bokonzi</title>
+</head>
+<body>        
+    <?php
+          include("app.php") ;          
+    ?>
+      <script src="js.js"></script>      
+  <?php
+       include("links.php") ;     
+   ?>
+</body>
+</html>
---
+++ section/section.php
@@ -0,0 +1,3 @@
+<?php 
+    include("section/section.html");
+?>
